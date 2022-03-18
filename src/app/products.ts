7eb0619cd3843177d995ebe46fa2068d012bf324
vitorf997 <vitorf997@gmail.com>
2022-03-18 16:18:33 +0000

    1 file modified

--- src/app/products.ts
+++ src/app/products.ts
@@ -23,6 +23,12 @@ export const products = [
     name: 'Phone Standard',
     price: 299,
     description: ''
+  },
+  {
+    id: 4,
+    name: 'XI phone',
+    price: 705,
+    description: 'Cheap and good'
   }
 ];
 
